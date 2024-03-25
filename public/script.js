0b47a706e8a5c944748c178f91bfd29d5292db5c
Tristan-Hudak <tristanj.hudak@gmail.com>
2024-03-25 19:42:08 -0400

    assignment14repo uploading

---
+++ public/script.js
@@ -0,0 +1,222 @@
+const getCrafts = async() => {
+    try{
+        return (await (await fetch("http://localhost:3000/api/crafts"))).json();
+    }
+    catch(error) {
+        console.log("error reciveing data");
+        return "";
+    }
+};
+
+const showCrafts = async() => {
+    const craftsJSON = await getCrafts();
+    //console.log(craftsJSON);
+    const craftDiv = document.getElementById("json-container");
+    if(craftDiv == ""){
+        craftDiv.innerHTML = "sorry, no Animals";
+        return;
+    }
+    const div1 = document.createElement("div");
+    const div2 = document.createElement("div");
+    const div3 = document.createElement("div");
+    const div4 = document.createElement("div");
+    const div5 = document.createElement("div");
+
+    const divConstructor = (currentDiv, num) => {
+        console.log(currentDiv);
+        currentDiv.setAttribute("id", "div"+num);
+        currentDiv.setAttribute("class", "img-size");
+        currentDiv.classList.add("class","coll1o5");
+        
+    }
+
+    const listConstrtor = (arraies) => {
+        const modalSecUl = document.createElement("ul");
+
+        arraies.forEach((array)=>{
+            const listItem = document.createElement("li");
+            listItem.innerHTML = array;
+            modalSecUl.append(listItem);
+        });
+
+        return modalSecUl;
+    }
+
+    divConstructor(div1,1);
+    divConstructor(div2,2);
+    divConstructor(div3,3);
+    divConstructor(div4,4);
+    divConstructor(div5,5);
+
+
+    craftDiv.append(div1);
+    craftDiv.append(div2);
+    craftDiv.append(div3);
+    craftDiv.append(div4);
+    craftDiv.append(div5);
+
+    let modalCounter = 0;
+
+    craftsJSON.forEach((craft) => {
+        const section = document.createElement("section");
+        section.classList.add("spacing");
+        section.setAttribute("rel", "modal "+modalCounter);
+
+        const img = document.createElement("img");
+        img.setAttribute("id", "img-holder");
+        img.setAttribute("rel", "modal "+modalCounter);
+        img.src = "http://localhost:3000/images/" + craft.image;
+
+        
+
+        //modal creation
+        const modalDiv = document.createElement("div");
+        const modalBody = document.createElement("div");
+
+        modalDiv.classList.add("modal")
+        modalDiv.setAttribute("id", "modal "+modalCounter);
+        modalDiv.classList.add("class", "show-hide");
+
+        
+
+        modalBody.classList.add("flex-box");
+        modalBody.classList.add("modal-content");
+
+        const modalExit = document.createElement("span");
+        modalExit.setAttribute("id","farRight")
+        modalExit.setAttribute("rel", "modal "+modalCounter);
+        modalExit.setAttribute("class","close")
+        modalExit.innerHTML =  `&times;`;
+
+        const modalImg = document.createElement("img");
+        modalImg.setAttribute("id", "img-modal");
+        modalImg.src = "http://localhost:3000/images/" + craft.image;
+
+        //text area
+
+        const modalSection = document.createElement("section");
+        modalSection.setAttribute("id", "sec-space")
+
+        const modalSecTitle = document.createElement("h1");
+        const modalSecDescrip = document.createElement("p");
+        const modalSecUlTitle = document.createElement("h2");
+        
+
+        modalSection.append(modalSecTitle);
+        modalSecTitle.innerHTML = craft.name;
+
+        modalSection.append(modalSecDescrip);
+        modalSecDescrip.innerHTML = craft.description;
+
+        modalSection.append(modalSecUlTitle);
+        modalSecUlTitle.innerHTML = "Supplies";
+
+        modalSection.append(listConstrtor(craft.supplies));
+
+        //order
+
+        const modalContent = document.createElement("div");
+        const modalHeader = document.createElement("div");
+
+        modalContent.setAttribute("class", "coll1o2");
+        modalContent.setAttribute("class", "flex-box");
+        modalHeader.setAttribute("class", "coll1o2");
+
+        modalHeader.append(modalExit);
+        modalContent.append(modalImg);
+        modalContent.append(modalSection);
+        
+        modalBody.append(modalContent);
+        modalBody.append(modalHeader);
+        
+
+
+        modalDiv.append(modalBody);
+
+
+        
+        //entering right div
+        
+
+        if(craft.number == 1){
+            div1.append(section);
+            //console.log(1);
+        }
+        else if(craft.number == 2){
+            div2.append(section);
+            //console.log(2);
+        }
+        else if(craft.number == 3){
+            div3.append(section);
+            //console.log(3);
+        }
+        else if(craft.number == 4){
+            div4.append(section);
+            //console.log(4);
+        }
+        else if(craft.number == 5){
+            div5.append(section);
+            //console.log(5);
+        }
+        else {
+            div1.append(section);
+            //console.log(0);
+        }
+
+        
+        //console.log(img.src);
+
+        section.append(img);
+        section.append(modalDiv);
+
+        modalCounter++;
+    });
+
+    
+
+    const showThatModal = (e) => {
+        
+
+        if( e.target.getAttribute("id") == "img-holder" || e.target.getAttribute("class") == "spacing" ){
+            //console.log(e.target.getAttribute("rel"));
+            const id = e.target.getAttribute("rel");
+            console.log(id + "Open");
+            document.getElementById(id).style.display = "block";
+
+        }
+        
+    };
+
+    const exitModal = (e) => {
+        //console.log(e.target.getAttribute("id"));
+        const modalsid = e.target.getAttribute("rel");
+        //console.log(modalsid + "close");
+        document.getElementById(modalsid).style.display = "none";
+    }
+
+
+    
+    
+    
+    const container = document.querySelector("#json-container");
+
+    const secMatches = container.querySelectorAll("section");
+    const spanMatches = container.querySelectorAll("span");
+
+    //console.log(matches);
+
+    secMatches.forEach((sec)=>{
+        //console.log(document.getElementById("exiter"));
+        sec.onclick = showThatModal;
+        
+    });
+
+    spanMatches.forEach((span)=>{
+        span.onclick = exitModal;
+    })
+    
+
+};
+
+showCrafts();
+
---
+++ server.js
@@ -0,0 +1,310 @@
+// npm init --yes
+// npm i express
+
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    console.log("soem one is launching");
+    res.sendFile( __dirname + "/index.html");
+});
+
+
+app.get("/api/crafts",(req,res)=>{
+    console.log("soem one is requesting the api");
+    const crafts = [];
+
+    crafts[0] = {
+        number: 1,
+        name: "Beaded JellyFish",
+        image: "bead-jellyfish.jpg",
+        description: "Create a hanging jellyfish using eggcartons and multicolored beads",
+        supplies: [
+          "string",
+          "egg cartons",
+          "beads"
+        ]
+    };
+    crafts[1] = {
+        number: 2,
+        name: "Character Bookmarks",
+        image: "bookmarks.jpeg",
+        description: "Create a little birdy bookmark to always remin you were you were",
+        supplies: [
+        "yellow construction paper",
+        "orange construction paper",
+        "black construction paper"
+        ]
+    };
+    crafts[2] = {
+        number: 3,
+        name: "Button Flowers",
+        image: "button-flowers.jpeg",
+        description: "Create a fun bouquet of flowers with your favorite buttons",
+        supplies: [
+          "multicolored buttons",
+          "multicolored flet",
+          "green straws",
+          "ribbon"
+        ]
+    };
+    crafts[3] = {
+        number: 4,
+        name: "Cheerio Necklaces",
+        image: "cheerio-necklace.webp",
+        description: "Create a fun and edible necklace",
+        supplies: [
+          "Cheerios or Fruit Loops",
+          "Elastic string"
+        ]
+    };
+    crafts[4] = {
+        number: 5,
+        name: "Beaded JellyFish",
+        image: "bead-jellyfish.jpg",
+        description: "Create a hanging jellyfish using eggcartons and multicolored beads",
+        supplies: [
+          "string",
+          "egg cartons",
+          "beads"
+        ]
+    };
+    crafts[5] = {
+        number: 1,
+        name: "Cotton Ball Cupcakes",
+        image: "cotton-ball-cupcakes.webp",
+        description: "Decorate your fun filled cupcake however you want.",
+        supplies: [
+            "Construction Paper",
+            "Cotton Balls",
+            "Black Sharpie",
+            "Glitter"
+        ]
+    };
+    crafts[6] = {
+        number: 2,
+        name: "Egg Carton Flowers",
+        image: "egg-carton-flowers.jpg",
+        description: "Make a beautiful bouquet with egg cartons and other items you can find around the house",
+        supplies: [
+            "Egg Cartons",
+            "Butons",
+            "Green Pipe Cleaner",
+            "Ribbon",
+            "Canvas"
+        ]
+    };
+    crafts[7] = {
+        number: 3,
+        name: "Finger Puppets",
+        image: "finger-puppets.jpeg",
+        description: "These little critters are easy to make, and will entertain your little one while they make a show.",
+        supplies: [
+            "Pom-poms",
+            "Googly Eyes",
+            "Pipe Cleaner"
+        ]
+    };
+    crafts[8] = {
+        number: 4,
+        name: "Ribbon Flower Headbands",
+        image: "flower-headbands.jpg",
+        description: "Let your little one show off her new style with these pretty and customizable headbands",
+        supplies: [
+            "Plain headband",
+            "Ribbon",
+            "Buttons",
+            "Gems"
+        ]
+    };
+    crafts[9] = {
+        number: 5,
+        name: "Hand Print Fish Puppets",
+        image: "handprint-fish.jpg",
+        description: "We all need to take every opportunity we can to remember those tiny hands, and what better way to do it, then to make fish puppets!",
+        supplies: [
+            "Popsicle sticks",
+            "Cardstock",
+            "Gems",
+            "Googly Eyes"
+        ]
+    };
+    crafts[10] = {
+        number: 1,
+        name: "Hand Print Tree",
+        image: "hand-print-tree.jpeg",
+        description: "This is a fun way to get your little one into finger painting.",
+        supplies: [
+            "Watercolor Paper",
+            "Finger paint"
+        ]
+    };
+    crafts[11] = {
+        number: 2,
+        name: "Melted Bead Bowl",
+        image: "melted-bead-bowl.jpeg",
+        description: "All they need to do is shape their faviorte design, warm it up and they have a brand new bowl.",
+        supplies: [
+            "Beads",
+            "Bowl",
+            "Parchment paper"
+        ]
+    };
+    crafts[12] = {
+        number: 3,
+        name: "Monster Kites",
+        image: "monster-rolls.jpg",
+        description: "Let's make those scary toilet paper rolls fly!",
+        supplies: [
+            "Toilet paper rolls",
+            "Paint",
+            "Tissue Paper",
+            "String"
+        ]
+    };
+    crafts[13] = {
+        number: 4,
+        name: "Pool Noodle Boats",
+        image: "noodle-boats.png",
+        description: "Let's make a boat that will actually float, due to the floating bottom of a pool noodle.",
+        supplies: [
+            "Pool Noodle",
+            "Straw",
+            "Plastic Paper"
+        ]
+    };
+    crafts[14] = {
+        number: 5,
+        name: "Paper Plate Bees",
+        image: "paper-plate-bees.jpeg",
+        description: "Let's have fun with making cute little bees, or big bees actually.",
+        supplies: [
+            "Paper Plate",
+            "Googly Eyes",
+            "Close Pins",
+            "Black pom poms",
+            "Yellow Paint",
+            "Black Paint"
+        ]
+    };
+    crafts[15] = {
+        number: 1,
+        name: "Paper Plate Dinosaurs",
+        image: "paper-plate-dinosaurs.jpg",
+        description: "Who would have thought that half a paper plate would be the base of a dinosaur.",
+        supplies: [
+            "Paper Plate",
+            "Paint",
+            "Close Pins",
+            "Construction Paper"
+        ]
+    };
+    crafts[16] = {
+        number: 2,
+        name: "Porcupine Leafs",
+        image: "porcupine-leaf.webp",
+        description: "Let's turn an ordinary paper plate into a fun filled mask.",
+        supplies: [
+            "Leafs",
+            "Berries",
+            "Acorns",
+            "Construction Paper"
+        ]
+    };
+    crafts[17] = {
+        number: 3,
+        name: "Rainbow Cloud",
+        image: "rainbow-cloud.webp",
+        description: "Some cotton and color and you'll have a beautiful rainbow.",
+        supplies: [
+            "Paper Plate",
+            "Cotton Balls",
+            "Construction Paper"
+        ]
+    };
+    crafts[18] = {
+        number: 4,
+        name: "Fun Shaped Crayons",
+        image: "shaped-crayons.jpg",
+        description: "Let's melt some crayons together and let them harden into fun shapes.",
+        supplies: [
+            "Broken Crayons",
+            "Mold"
+        ]
+    };
+    crafts[19] = {
+        number: 5,
+        name: "Straw Farris Wheel",
+        image: "straw-faris-wheel.jpg",
+        description: "It might be too small to ride, but this farris wheel is the most colorful of all.",
+        supplies: [
+            "Multicolored straws",
+            "Platform"
+        ]
+    };
+    crafts[20] = {
+        number: 1,
+        name: "Sunny String",
+        image: "sun-string.jpg",
+        description: "Let's practice our fine motor skills while we weave the string into a fun sun.",
+        supplies: [
+            "Yellow String",
+            "Paper Plate",
+            "Yellow construction paper",
+            "Yellow and Orange beads"
+        ]
+    };
+    crafts[21] = {
+        number: 2,
+        name: "Tissue Ballerinas",
+        image: "tisue-dancer.jpeg",
+        description: "These beautiful dancers will look great on display",
+        supplies: [
+            "Pipe cleaner",
+            "Tissue Paper",
+            "Elastics"
+    ]
+    };
+    crafts[22] = {
+        number: 3,
+        name: "Toilet Paper Roll Animals",
+        image: "toilet-paper-animals.jpeg",
+        description: "These beautiful dancers will look great on display",
+        supplies: [
+            "Toilet Paper Rolls",
+            "Construction Paper",
+            "Googly Eyes"
+        ]
+    };
+    crafts[23] = {
+        number: 4,
+        name: "Toilet Paper Butterfly",
+        image: "toilet-paper-butterfly.jpg",
+        description: "Such a sweat little flyer",
+        supplies: [
+            "Toilet Paper Rolls",
+            "Construction Paper",
+            "Googly Eyes",
+            "Buttons"
+        ]
+    };
+    crafts[24] = {
+        number: 5,
+        name: "Valentines Jar",
+        image: "valentines-jar.webp",
+        description: "So much hearts all in one",
+        supplies: [
+            "Clay",
+            "Glitter"
+        ]
+    };
+
+    console.log(crafts);
+    res.json(crafts);
+});
+
+
+app.listen(3000),()=>{
+    console.log("listening");
+}
